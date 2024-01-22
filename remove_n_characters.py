a77078092ac46bd49652e45725652da707154475
melissasalcedo <salcedomelissa189@gmil.com>
2024-01-22 16:47:34 +0800

    Done testing

---
+++ remove_n_characters.py
@@ -0,0 +1,9 @@
+# Remove first n characters from a string
+
+# Function
+def remove_chars(origstring ):
+    print('Original string:', origstring)
+    
+
+# Test cases
+print("Removing characters from a string")
