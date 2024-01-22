ba615a00ddd1ae74b5b7deb3576a0296ecebcbc4
melissasalcedo <salcedomelissa189@gmil.com>
2024-01-22 16:59:40 +0800

    Finished

--- remove_n_characters.py
+++ remove_n_characters.py
@@ -1,9 +1,14 @@
 # Remove first n characters from a string
 
+print("Removing characters from a string")
+
 # Function
-def remove_chars(origstring ):
+def remove_chars(origstring, number_to_remove ):
     print('Original string:', origstring)
+    result_string = origstring[number_to_remove:]
+    return result_string
     
 
 # Test cases
-print("Removing characters from a string")
+print(remove_chars("mishkamushka", 9))
+print(remove_chars("mishkamushka", 3))
